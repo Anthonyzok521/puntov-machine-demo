f6501e00ce258beea066b79d20e59feb446d2e87
Anthonyzok521 <anthony@acteam.dev>
2025-07-29 14:19:10 -0400

    feat: apply coupon

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"18818009497c581ede5d8a3b8b833b81d00ceb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1680504505"
+    serviceWorkerVersion: "3184976730"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -2192,14 +2192,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "cfa2f02a7dae7cb1fae31100172b52d1
 "canvaskit/skwasm.wasm": "1c93738510f202d9ff44d36a4760126b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "2d401c882ee73b0ed6b2a20ba5ea1051",
+"flutter_bootstrap.js": "d6843656cffb6f849181082d34d7c801",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "7f8540a78fb14c7390042bc5048ea2d0",
 "/": "7f8540a78fb14c7390042bc5048ea2d0",
-"main.dart.js": "f999f26b86c632e50e5607fc8e79fa09",
+"main.dart.js": "466b7a88b87c0aeffba86701bdd18923",
 "manifest.json": "411241b7bd7f1b0492d5fea6e2b64164",
 "version.json": "cf360e8d61e2169468c498b5cd9c816d"};
 // The application shell files that are downloaded before a service worker can
